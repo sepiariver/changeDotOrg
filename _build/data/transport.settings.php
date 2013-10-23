2427bfa5ad5dbeb37391ad07d5c6adbb7719fd9b
sepiariver <info@sepiariver.com>
2013-10-23 15:18:10 -0700

    Initial commit of files from client config

---
+++ _build/data/transport.settings.php
@@ -0,0 +1,33 @@
+<?php
+
+$s = array(
+    'admin_groups' => 'Administrator',
+    'clear_cache' => true,
+    'vertical_tabs' => false,
+    'google_fonts_api_key' => ''
+);
+
+$settings = array();
+
+foreach ($s as $key => $value) {
+    if (is_string($value) || is_int($value)) { $type = 'textfield'; }
+    elseif (is_bool($value)) { $type = 'combo-boolean'; }
+    else { $type = 'textfield'; }
+
+    $parts = explode('.',$key);
+    if (count($parts) == 1) { $area = 'Default'; }
+    else { $area = $parts[0]; }
+    
+    $settings['clientconfig.'.$key] = $modx->newObject('modSystemSetting');
+    $settings['clientconfig.'.$key]->set('key', 'clientconfig.'.$key);
+    $settings['clientconfig.'.$key]->fromArray(array(
+        'value' => $value,
+        'xtype' => $type,
+        'namespace' => 'clientconfig',
+        'area' => $area
+    ));
+}
+
+return $settings;
+
+
