074ea9b5abd951c5e61cb7da355e03a0d60528dd
sepiariver <info@sepiariver.com>
2013-10-23 15:25:44 -0700

    Cleanup and convert initial copied files

--- core/components/changedotorg/model/changedotorg/changedotorg.class.php
+++ core/components/changedotorg/model/changedotorg/changedotorg.class.php
@@ -1,29 +1,28 @@
 <?php
 /**
- * ClientConfig
+ * changeDotOrg
  *
- * Copyright 2011 by Mark Hamstra <hello@markhamstra.com>
+ * Copyright 2013 by YJ TSo <yj@modx.com>
  *
- * This file is part of ClientConfig, a real estate property listings component
- * for MODX Revolution.
+ * This file is part of changeDotOrg, a MODX integration of the Change.org API.
  *
- * ClientConfig is free software; you can redistribute it and/or modify it under
+ * changeDotOrg is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free Software
  * Foundation; either version 2 of the License, or (at your option) any later
  * version.
  *
- * ClientConfig is distributed in the hope that it will be useful, but WITHOUT ANY
+ * changeDotOrg is distributed in the hope that it will be useful, but WITHOUT ANY
  * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
  * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with
- * ClientConfig; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+ * changeDotOrg; if not, write to the Free Software Foundation, Inc., 59 Temple Place,
  * Suite 330, Boston, MA 02111-1307 USA
  *
- * @package clientconfig
+ * @package changedotorg
 */
 
-class ClientConfig {
+class changeDotOrg {
     /**
      * @var modX|null $modx
      */
@@ -45,30 +44,18 @@ class ClientConfig {
     function __construct(modX &$modx,array $config = array()) {
         $this->modx =& $modx;
 
-        $corePath = $this->modx->getOption('clientconfig.core_path',$config,$this->modx->getOption('core_path').'components/clientconfig/');
-        $assetsUrl = $this->modx->getOption('clientconfig.assets_url',$config,$this->modx->getOption('assets_url').'components/clientconfig/');
-        $assetsPath = $this->modx->getOption('clientconfig.assets_path',$config,$this->modx->getOption('assets_path').'components/clientconfig/');
+        $corePath = $this->modx->getOption('core_path').'components/changedotorg/');
         $this->config = array_merge(array(
             'basePath' => $corePath,
             'corePath' => $corePath,
             'modelPath' => $corePath.'model/',
-            'processorsPath' => $corePath.'processors/',
             'elementsPath' => $corePath.'elements/',
-            'templatesPath' => $corePath.'templates/',
-            'assetsPath' => $assetsPath,
-            'jsUrl' => $assetsUrl.'js/',
-            'cssUrl' => $assetsUrl.'css/',
-            'assetsUrl' => $assetsUrl,
-            'connectorUrl' => $assetsUrl.'connector.php',
-
-            'verticalTabs' => $this->modx->getOption('clientconfig.vertical_tabs', null, false),
         ),$config);
 
         $modelPath = $this->config['modelPath'];
-        $this->modx->addPackage('clientconfig',$modelPath);
-        $this->modx->lexicon->load('clientconfig:default');
+        $this->modx->addPackage('changedotorg',$modelPath);
         
-        $this->debug = (bool)$this->modx->getOption('clientconfig.debug',null,false);
+        $this->debug = (bool)$this->modx->getOption('changedotorg.debug',null,false);
     }
 
     /**
